9e00cc05970b45eb78e7db305c3d756d51f49af4
Ilham Muttaqien <imuttaqien17@gmail.com>
2025-03-06 00:13:32 +0700

    fix: roll file name log
    Signed-off-by: Ilham Muttaqien <imuttaqien17@gmail.com>

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,7 +2,6 @@ import env from '@config/index';
 import type { PinoLoggerOptions } from 'fastify/types/logger';
 import { join } from 'path';
 import { type TransportPipelineOptions } from 'pino';
-import { useDayjs } from './dayjs';
 
 export const loggerOptions: PinoLoggerOptions = {
   transport: {
@@ -19,9 +18,10 @@ export const loggerOptions: PinoLoggerOptions = {
             level: 'error',
             target: 'pino-roll',
             options: {
-              file: join(process.cwd(), `/logs/${useDayjs().format('YYYY-MM-DD')}`),
+              file: join(process.cwd(), '/logs/error'),
               frequency: 'daily',
               extension: '.log',
+              dateFormat: 'yyyy-MM-dd',
               mkdir: true
             }
           }
